fd6a5eda6df1bb25ee287336027ff83ed971763c
Jacob Feuerbach <jacob.feuerbach@gmail.com>
2018-09-07 15:53:04 -0500

    Updated libraries
    -Angular and CLI to 6+
    -Removed FontAwesome and using SVG for icon
    -Updated NgBootstrap to 3+

--- src/date-range-picker/date-range-picker/date-range-picker.component.ts
+++ src/date-range-picker/date-range-picker/date-range-picker.component.ts
@@ -1,20 +1,37 @@
-import { Component, OnInit, Input, Output, EventEmitter, ElementRef, AfterViewInit, ViewChild } from '@angular/core';
-import { NgbDateStruct, NgbCalendar, NgbInputDatepicker, NgbDateParserFormatter } from '@ng-bootstrap/ng-bootstrap';
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter,
+  ElementRef,
+  AfterViewInit,
+  ViewChild
+} from '@angular/core';
+import {
+  NgbDateStruct,
+  NgbCalendar,
+  NgbInputDatepicker,
+  NgbDateParserFormatter
+} from '@ng-bootstrap/ng-bootstrap';
 import { DateRange } from '../date-range';
 import { NgbDateNativeAdapter } from '../services/NgbDateNativeAdapter';
 import { equals, before, after, format } from '../services/NgbDateStructUtils';
-import { NgbDate } from '@ng-bootstrap/ng-bootstrap/datepicker/ngb-date';
 
 @Component({
   selector: 'date-range-picker',
   templateUrl: './date-range-picker.component.html',
   styleUrls: ['./date-range-picker.component.css']
 })
-export class DateRangePickerComponent implements OnInit, AfterViewInit {
-  @Input() dateRange: DateRange;
-  @Input() minDate?: Date;
-  @Input() maxDate?: Date;
-  @Output() dateRangeChange = new EventEmitter<DateRange>();
+export class DateRangePickerComponent implements OnInit {
+  @Input()
+  dateRange: DateRange;
+  @Input()
+  minDate?: Date;
+  @Input()
+  maxDate?: Date;
+  @Output()
+  dateRangeChange = new EventEmitter<DateRange>();
   hoveredDate: NgbDateStruct;
 
   fromDate: NgbDateStruct;
@@ -22,9 +39,13 @@ export class DateRangePickerComponent implements OnInit, AfterViewInit {
   min: NgbDateStruct | null;
   max: NgbDateStruct | null;
   onFirstSelection = true;
-  @ViewChild('dp', {read: ElementRef}) private inputElRef: ElementRef;
+  @ViewChild('dp', { read: ElementRef })
+  private inputElRef: ElementRef;
 
-  constructor(private readonly dateAdapter: NgbDateNativeAdapter, private elRef: ElementRef) {}
+  constructor(
+    private readonly dateAdapter: NgbDateNativeAdapter,
+    private elRef: ElementRef
+  ) {}
 
   ngOnInit() {
     this.fromDate = this.dateAdapter.fromModel(this.dateRange.start);
@@ -32,10 +53,7 @@ export class DateRangePickerComponent implements OnInit, AfterViewInit {
     this.min = this.minDate ? this.dateAdapter.fromModel(this.minDate) : null;
     this.max = this.maxDate ? this.dateAdapter.fromModel(this.maxDate) : null;
   }
-  ngAfterViewInit() {
-    this.input = this.elRef.nativeElement.querySelector('input');
-    this.input.value = this.formatInputText();
-  }
+
   onDateChange(date: NgbDateStruct, dp: NgbInputDatepicker) {
     if (!this.fromDate && !this.toDate) {
       this.fromDate = date;
@@ -55,7 +73,9 @@ export class DateRangePickerComponent implements OnInit, AfterViewInit {
   }
 
   formatInputText() {
-    return `${this.fromDate ? format(this.fromDate) : ''} - ${this.toDate ? format(this.toDate) : ''}`;
+    return `${this.fromDate ? format(this.fromDate) : ''} - ${
+      this.toDate ? format(this.toDate) : ''
+    }`;
   }
 
   isHovered = date =>
@@ -63,7 +83,7 @@ export class DateRangePickerComponent implements OnInit, AfterViewInit {
     !this.toDate &&
     this.hoveredDate &&
     after(date, this.fromDate) &&
-    before(date, this.hoveredDate)
+    before(date, this.hoveredDate);
 
   isInside = date => after(date, this.fromDate) && before(date, this.toDate);
   isFrom = date => equals(date, this.fromDate);
@@ -71,7 +91,7 @@ export class DateRangePickerComponent implements OnInit, AfterViewInit {
   isWeekend = date => {
     const d = new Date(date.year, date.month - 1, date.day);
     return d.getDay() === 0 || d.getDay() === 6;
-  }
+  };
   isDisabled = date => after(date, this.max) || before(date, this.min);
   isInFuture = date => after(date, this.toDate);
 }
